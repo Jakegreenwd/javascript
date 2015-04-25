fe6ee15571ae32ffe3a24d09401f71a71c1a6e83
Jake Greenwood <j@jakegreenwood.com>
2015-04-25 14:42:54 -0400

    JS

---
+++ HR/Callback Examples/calcISP.js
@@ -0,0 +1,82 @@
+var engineAndISP = []; //declaring empty arrays 
+var engine = []; 
+ 
+
+function Engine(name,exhastSpeed) { //constructor used to input engine parameters
+	this.name = name;
+	this.exhastSpeed = exhastSpeed;
+	this.isp = 0;
+}
+
+function calcISP(engine,callback,options) { //function to calculate ISP. Takes in an engine object, a callback function to determine what to do, and any options on that callback function
+	engine.isp = Math.round(engine.exhastSpeed / 9.81); //simple calculation to get ISP of engine and storing the result in the objects isp parameter
+	if(!options) {
+		callback(engine.name + " Isp: " + engine.isp + " seconds"); //if no option parameter is called (in the case of the print function) omit the options parameter
+	}
+	else {
+		callback(options,engine.name + " Isp: " + engine.isp + " seconds"); //else include and read the options parameter
+	}	
+}
+
+function print(param) { //print whatever parameter is passed to it out to the console
+	console.log(param);
+}
+
+function toArray(array,param) { //push whatever parameter is passed to it to the array specified
+	array.push(param);
+	
+}
+
+function printArray(param) { //print the contents of an array line by line
+	for(var i=0; i<param.length; i++) {
+		print(param[i]);
+	}
+}
+
+function logArray(engineArray) { //take the contents of a specified array and pass it into the calcISP function, then log the results into the engineAndISP array
+	for(var i=0; i<engineArray.length; i++) {
+		calcISP(engineArray[i],toArray,engineAndISP);
+	}
+	printArray(engineAndISP); //print the engineAndISP array to the console
+	console.log("\n"); // line break (just to make everything look clean)
+}
+
+engine[0] = new Engine("Space Shuttle Main Engine",4400); //passing parameters into the Engine object contructor (name and exhast speed)
+engine[1] = new Engine("Solid Rocket Booster",2500); 
+engine[2] = new Engine("Ion Thruster",29000); 
+engine[3] = new Engine("Nuclear Thermal Rocket",8300); 
+
+var ssme = engine[0]; //setting objects in the engine array equal to variable names for easier access when using print callback function
+var srb = engine[1];
+var ion = engine[2];
+var nuclear = engine[3];
+
+logArray(engine); //calling the logArray function the the console
+
+calcISP(ion,print); //caling the calcISP function with the print callback function to log it to the console
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- HR/Callback Examples/spacecompanies.js
+++ HR/Callback Examples/spacecompanies.js
@@ -1,40 +1,40 @@
-var launchCompanies = ["SpaceX", "ULA", "Orbital Sceinces", "Arianespace"];
+var launchCompanies = ["SpaceX", "ULA", "Orbital Sceinces", "Arianespace"]; //declaring arrays for launch companies, launch vehicles, and an empty array for later use
 var launchVehicles = ["Falcon 9", "Delta IV", "Antares", "Ariane 5"];
 var companyAndVehicle = [];
 
-function print(name) {
+function print(name) { //print whatever parameter is passed into it out to the console
 	console.log(name);
 }
 
-function printArray(name) {
+function printArray(name) { //print the contents of a specifed array to the console line by line
 	for(var i=0; i<name.length; i++) {
 		console.log(name[i]);
 	}
 }
 
-function addTo(name) {
+function addTo(name) { //push the parameter that is passed into it to the comnpanyAndVehicle array
 	companyAndVehicle.push(name);	
 }
 
 
-function spaceSystem(company, callback) {
-	for(var i=0; i<launchCompanies.length; i++) {
-		if(company === launchCompanies[i]) {
-			callback(launchCompanies[i] + ":" + " " + launchVehicles[i]);
+function spaceSystem(company, callback) { //function that finds the corresponding value in the launchVehicles array of the company parameter that was passed into it
+	for(var i=0; i<launchCompanies.length; i++) { //cycles through array 
+		if(company === launchCompanies[i]) { //if company parameter matches a value in launchCompanies
+			callback(launchCompanies[i] + ":" + " " + launchVehicles[i]); //callback function on the result of combining a value from launchCompanies with corresponding launchVehicles
 		}
 	}
 
 }
 
 
-function logAllCraft() {
+function logAllCraft() { //pass the values from launchCompanies into the spaceSystem function to find corresponding values in launchVehicle
 	for(var i=0; i<launchCompanies.length; i++) {
-		spaceSystem(launchCompanies[i],addTo);
+		spaceSystem(launchCompanies[i],addTo); //log results into companyAndVehicle array
 	}
-	printArray(companyAndVehicle);
+	printArray(companyAndVehicle); //print companyAndVehicle array to the console
 }
 
 
-spaceSystem("ULA",print);
+spaceSystem("ULA",print); //call spaceSystem function with the print callback function
 
-logAllCraft();
+logAllCraft(); //call logAllCraft function
